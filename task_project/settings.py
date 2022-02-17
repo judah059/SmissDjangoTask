58b0233ba449184e55ee320603bb00af9cb7c8a0
Danylo Pohrebniak <danylo.pohrebniak@smissltd.com>
2022-02-17 17:04:44 +0200

    docker things

--- task_project/settings.py
+++ task_project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'task_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_NAME'),
+        'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'pg_db',
